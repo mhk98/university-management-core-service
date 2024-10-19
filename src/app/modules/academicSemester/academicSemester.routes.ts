afe230b72b0dcc51a9ad921ecb48fba39e571214
KHAtarnak@@98 <mohsinkabirseo@gmail.com>
2024-10-19 18:22:39 +0600

    latest

--- src/app/modules/academicSemester/academicSemester.routes.ts
+++ src/app/modules/academicSemester/academicSemester.routes.ts
@@ -1,6 +1,4 @@
 import express from 'express';
-import { ENUM_USER_ROLE } from '../../../enums/user';
-import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { AcademicSemeterController } from './academicSemster.controller';
 import { AcademicSemesterValidation } from './academicSemster.validation';
@@ -18,13 +16,13 @@ router.post(
 router.patch(
     '/:id',
     validateRequest(AcademicSemesterValidation.update),
-    auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+    // auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
     AcademicSemeterController.updateOneInDB
 );
 
 router.delete(
     '/:id',
-    auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+    // auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
     AcademicSemeterController.deleteByIdFromDB
 );
 
